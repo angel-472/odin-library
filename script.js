eb52330596aba7171b27c224faa9ea83ce096cea
angel <108492882+angel-472@users.noreply.github.com>
2023-03-06 17:20:59 -0400

    Refactor library to be contained inside a class

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-const btnAddBook = document.getElementById("add-book");
-const booksContainer = document.getElementById("books");
-
-const btnEditorSave = document.getElementById("book-editor-save");
-const btnEditorClose = document.getElementById("book-editor-close");
-const bookEditor = document.getElementById("book-editor");
-const txtEditorTitle = document.getElementById("book-editor-title");
-const txtEditorAuthor = document.getElementById("book-editor-author");
-
-let books = {};
 const statuses = {
   0: 'Unread',
   1: 'Reading',
   2: 'Completed',
 }
 
-btnAddBook.addEventListener("click", (e) => {
-  bookEditor.style.display = "flex";
-
-  //resets inputs
-  txtEditorTitle.value = "";
-  txtEditorAuthor.value = "";
-});
-btnEditorSave.addEventListener("click", (e) => {
-  let title = txtEditorTitle.value;
-  let author = txtEditorAuthor.value;
-  bookEditor.style.display = "none";
-  if(title == "" || author == ""){
-    return;
-  }
-  addBookToLibrary(title,author,0);
-});
-btnEditorClose.addEventListener("click", (e) => {
-  bookEditor.style.display = "none";
-});
 
 class Book {
   constructor(title, author, status){
@@ -43,78 +14,113 @@ class Book {
   }
 }
 
-function addBookToLibrary(title, author, status){
-  let book = new Book(title,author,status);
-  books[book.id] = book;
-  let element = getNewBookElement(book);
-  booksContainer.appendChild(element);
-  book.element = element;
-}
+class LibraryApp {
+  constructor(){
+    this.books = {};
 
-function getNewBookElement(book){
-  let element = document.createElement("div");
-  element.classList.add("book");
-  element.innerHTML = `
-  <img class="book-icon" src="https://api.dicebear.com/5.x/icons/svg?seed=${book.title}">
-  <div class="book-info">
-    <p class="book-title">${book.title}</p>
-    <p class="book-author">By: ${book.author}</p>
-  </div>
-  <div class="book-buttons">
-    <button data-book="${book.id}" class="button button-status status-${book.status}">${statuses[book.status]}</button>
-    <button data-book="${book.id}" class="button button-remove">Remove</button>
-  </div>
-  `
-  return element;
-}
+    this.btnAddBook = document.getElementById("add-book");
+    this.booksContainer = document.getElementById("books");
 
-document.addEventListener('click', ({ target }) => {
-  if (target.matches('button')) {
-    let bookId = target.getAttribute('data-book');
-    if (bookId !== undefined){
-      let book = books[bookId];
-      if(target.classList.contains("button-remove")){
-        console.log("remove book " + bookId);
-        book.element.remove();
-        delete books[bookId];
+    this.btnEditorSave = document.getElementById("book-editor-save");
+    this.btnEditorClose = document.getElementById("book-editor-close");
+    this.bookEditor = document.getElementById("book-editor");
+    this.txtEditorTitle = document.getElementById("book-editor-title");
+    this.txtEditorAuthor = document.getElementById("book-editor-author");
+    this.divNoBooks = document.getElementById("no-books");
+  }
+  init(){
+    this.btnAddBook.addEventListener("click", (e) => {
+      this.bookEditor.style.display = "flex";
+
+      //resets inputs
+      this.txtEditorTitle.value = "";
+      this.txtEditorAuthor.value = "";
+    });
+
+    this.btnEditorSave.addEventListener("click", (e) => {
+      let title = this.txtEditorTitle.value;
+      let author = this.txtEditorAuthor.value;
+      this.bookEditor.style.display = "none";
+      if(title == "" || author == ""){
+        return;
       }
-      else if(target.classList.contains("button-status")){
-        console.log("change status book " + bookId);
-        let status = book.status;
-        let newStatus = book.status + 1 < Object.keys(statuses).length ? book.status + 1 : 0;
-        target.classList.remove("status-" + book.status);
-        target.classList.add("status-" + newStatus);
-        book.status = newStatus;
-        target.innerText = statuses[newStatus];
+      addBookToLibrary(title,author,0);
+    });
+
+    this.btnEditorClose.addEventListener("click", (e) => {
+      this.bookEditor.style.display = "none";
+    });
+
+    document.addEventListener('click', ({ target }) => {
+      if (target.matches('button')) {
+        let bookId = target.getAttribute('data-book');
+        if (bookId !== undefined){
+          let book = this.books[bookId];
+          if(target.classList.contains("button-remove")){
+            console.log("remove book " + bookId);
+            book.element.remove();
+            delete this.books[bookId];
+          }
+          else if(target.classList.contains("button-status")){
+            console.log("change status book " + bookId);
+            let status = book.status;
+            let newStatus = book.status + 1 < Object.keys(statuses).length ? book.status + 1 : 0;
+            target.classList.remove("status-" + book.status);
+            target.classList.add("status-" + newStatus);
+            book.status = newStatus;
+            target.innerText = statuses[newStatus];
+          }
+        }
       }
-    }
-  }
-});
+    });
 
-function saveLocalStorage(){
-  window.localStorage.setItem('library_readinglist', JSON.stringify(books));
+    this.loadLocalStorage();
+    setInterval(() => this.saveLocalStorage(), 1000);
+
+    //no books div
+    setInterval(() => {
+      if(Object.keys(this.books).length < 1 && this.divNoBooks.style.display !== 'flex'){
+        this.divNoBooks.style.display = 'flex';
+      }
+      else if(Object.keys(this.books).length > 0 && this.divNoBooks.style.display !== 'none') {
+        this.divNoBooks.style.display = 'none';
+      }
+    }, 100);
+  }
+  addBook(title, author, status){
+    let book = new Book(title,author,status);
+    books[book.id] = book;
+    let element = this.getNewBookElement(book);
+    this.booksContainer.appendChild(element);
+    book.element = element;
+  }
+  getNewBookElement(book){
+    let element = document.createElement("div");
+    element.classList.add("book");
+    element.innerHTML = `
+    <img class="book-icon" src="https://api.dicebear.com/5.x/icons/svg?seed=${book.title}">
+    <div class="book-info">
+      <p class="book-title">${book.title}</p>
+      <p class="book-author">By: ${book.author}</p>
+    </div>
+    <div class="book-buttons">
+      <button data-book="${book.id}" class="button button-status status-${book.status}">${statuses[book.status]}</button>
+      <button data-book="${book.id}" class="button button-remove">Remove</button>
+    </div>
+    `
+    return element;
+  }
+  saveLocalStorage(){
+    window.localStorage.setItem('library_readinglist', JSON.stringify(this.books));
+  }
+  loadLocalStorage(){
+    if(window.localStorage.getItem('library_readinglist') == undefined) return;
+    let savedBooks = JSON.parse(window.localStorage.getItem('library_readinglist'));
+    Object.values(savedBooks).forEach((book) => {
+      library.addBook(book.title, book.author, book.status);
+    });
+  }
 }
 
-function loadLocalStorage(){
-  if(window.localStorage.getItem('library_readinglist') == undefined) return;
-  let savedBooks = JSON.parse(window.localStorage.getItem('library_readinglist'));
-  Object.values(savedBooks).forEach((book) => {
-    addBookToLibrary(book.title, book.author, book.status);
-  });
-}
-
-//runs them
-loadLocalStorage();
-setInterval(() => saveLocalStorage(), 1000);
-
-let noBooks = true;
-const divNoBooks = document.getElementById("no-books");
-//no books div
-setInterval(() => {
-  if(Object.keys(books).length < 1 && divNoBooks.style.display !== 'flex'){
-    divNoBooks.style.display = 'flex';
-  }
-  else if(Object.keys(books).length > 0 && divNoBooks.style.display !== 'none') {
-    divNoBooks.style.display = 'none';
-  }
-}, 100);
+const library = new LibraryApp();
+library.init();
